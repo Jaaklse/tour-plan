40b48d563d008229b9cb1beb3df09ffbc50d2736
Jaaklse <yakov_yastrebov@mail.ru>
2022-11-15 13:53:11 +0300

    create: improved reviews

--- js/main.js
+++ js/main.js
@@ -21,8 +21,8 @@ const reviewsSlider = new Swiper('.reviews-slider', {
 
   // Navigation arrows
   navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
   },
   keyboard: {
     enabled: true,
