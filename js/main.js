07fdda88c00b978e068ff5a336f2b6acd4c2c59f
Jaaklse <yakov_yastrebov@mail.ru>
2022-12-23 23:34:53 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -29,3 +29,9 @@ const reviewsSlider = new Swiper('.reviews-slider', {
     onlyInViewport: false,
   },
 });
+
+var menuButton = document.querySelector('.menu-button');
+menuButton.addEventListener('click', function() {
+  console.log('Клик по кнопке меню');
+  document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible');
+});
